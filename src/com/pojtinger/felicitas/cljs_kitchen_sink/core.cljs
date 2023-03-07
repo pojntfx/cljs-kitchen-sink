2d3ee8219016c0d30d52baa901caa975db405d41
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-03-07 23:32:30 +0100

    fix: Prevent re-creating React root on hot-reload

--- src/com/pojtinger/felicitas/cljs_kitchen_sink/core.cljs
+++ src/com/pojtinger/felicitas/cljs_kitchen_sink/core.cljs
@@ -1,6 +1,8 @@
 (ns com.pojtinger.felicitas.cljs-kitchen-sink.core
-  (:require [com.pojtinger.felicitas.cljs-kitchen-sink.components :as components]
+  (:require [com.pojtinger.felicitas.cljs-kitchen-sink.components :refer [index]]
             [reagent.dom.client :as client]))
 
+(defonce root (client/create-root (js/document.getElementById "root")))
+
 (defn ^:export ^:dev/after-load main []
-  (client/render (client/create-root (js/document.getElementById "root")) [components/home]))
+  (client/render root [index]))
